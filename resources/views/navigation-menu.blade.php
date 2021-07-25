3b62e1bb9ea8d423b43cfb993bbb401f684eac7e
Halil İbrahim BELKIR <ibrahim.belkir@tardigrad.com.tr>
2021-07-25 17:09:49 +0300

    en to tr

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -105,11 +105,11 @@
                         <x-slot name="content">
                             <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
+                                {{ __('Hesabı Yönet') }}
                             </div>
 
                             <x-jet-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                                {{ __('Profil') }}
                             </x-jet-dropdown-link>
 
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -127,7 +127,7 @@
                                 <x-jet-dropdown-link href="{{ route('logout') }}"
                                          onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                    {{ __('Log Out') }}
+                                    {{ __('Çıkış Yap') }}
                                 </x-jet-dropdown-link>
                             </form>
                         </x-slot>
